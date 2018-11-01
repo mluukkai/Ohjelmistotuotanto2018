563f99d85d5fc7d3dbede024ecbe2ef2996a2c84
Daniel Koch <daniel.koch@helsinki.fi>
2018-11-01 15:32:17 +0200

    Poistin nuo esimerkit :))

--- koodi/viikko1/RiippuvuuksienInjektointi1/src/main/java/ohtu/Main.java
+++
@@ -1,9 +0,0 @@
-package ohtu;
-
-import ohtu.laskin.KonsoliIO;
-
-public class Main {
-    public static void main(String[] args) {
-        new ohtu.laskin.Laskin(new KonsoliIO()).suorita();
-    }
-}
