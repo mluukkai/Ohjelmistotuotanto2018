88b33e8603db968e16c491a28aacf5a2f62ed989
Susanna Rajamäki <strajama@gmail.com>
2018-11-02 14:40:58 +0200

    lisätty ehto NHL-tehtävän topscorers-metodin while-loopiin

--- koodi/viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ koodi/viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -41,7 +41,7 @@ public class Statistics {
         ArrayList<Player> topScorers = new ArrayList<Player>();
         Iterator<Player> playerIterator = players.iterator();
         
-        while (howMany>=0) {
+        while (howMany>=0 && playerIterator.hasNext()) {
             topScorers.add( playerIterator.next() );            
             howMany--;
         }
