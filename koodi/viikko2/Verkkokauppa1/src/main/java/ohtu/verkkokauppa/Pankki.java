aa840ff095511ade00065906b460044f5bbcb408
caeh17 <phileth@protonmail.com>
2018-11-11 20:47:04 +0200

    Pieni korjaus

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -18,7 +18,7 @@ public class Pankki {
     }
 
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
-        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tililta + " tilille " + tilille
                 + " viite " + viitenumero + " summa " + summa + "e");
 
         // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
